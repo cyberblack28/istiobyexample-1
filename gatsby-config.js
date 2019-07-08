754d196eddca347cb133fac4f2d77d5b893b6653
askmeegs <megan037@gmail.com>
2019-07-08 12:31:20 -0400

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-41949399-6`,
       },
     },
     `gatsby-plugin-feed`,
